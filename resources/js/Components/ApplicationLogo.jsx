2989d3206e9ae1e9b8be8bb6130d2e397ebfdd3d
Yeferson Giraldo <104454063+ygiraldo-joonik@users.noreply.github.com>
2025-01-24 20:19:32 -0500

    feat: add llogo and decorations

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.jpg'
+import logo from '../../assets/logo.png'
 export default function ApplicationLogo(props) {
     return (
         <img src={logo} alt="Logo" {...props} />
