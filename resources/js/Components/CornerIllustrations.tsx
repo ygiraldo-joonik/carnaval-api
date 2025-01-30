4a8e060bfb46e109f37b1bba543cad4b4c7f644a
Yeferson Giraldo <104454063+ygiraldo-joonik@users.noreply.github.com>
2025-01-30 16:59:02 -0500

    feat: set position fixed to corner illustrations

--- resources/js/Components/CornerIllustrations.tsx
+++ resources/js/Components/CornerIllustrations.tsx
@@ -4,10 +4,10 @@ import corner2 from "../../assets/corner-2.png";
 export default function CornerIllustrations() {
     return (
         <>
-            <div className="corner-illustration left z-index-n1 absolute bottom-0 left-0 w-1/2 h-1/2 -mt-16 -ml-16 scale-50">
+            <div className="corner-illustration left z-index-n1 fixed bottom-0 left-0 w-1/2 h-1/2 -mt-16 -ml-16 scale-50">
                 <img src={corner1} alt="Illustration Corner 1" />
             </div>
-            <div className="corner-illustration right z-index-n1 absolute bottom-0 right-0 w-1/2 h-1/2 -mb-16 -mr-16 scale-50">
+            <div className="corner-illustration right z-index-n1 fixed bottom-0 right-0 w-1/2 h-1/2 -mb-16 -mr-16 scale-50">
                 <img src={corner2} alt="Illustration Corner 2" />
             </div>
         </>
