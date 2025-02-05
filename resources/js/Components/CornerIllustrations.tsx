1b94a7152f9e1618ba2a8db986ea5907f7e8c274
Yeferson Giraldo <104454063+ygiraldo-joonik@users.noreply.github.com>
2025-02-05 17:06:54 -0500

    feat: fix corner illustrations and add name of ssh key in action yml

--- resources/js/Components/CornerIllustrations.tsx
+++ resources/js/Components/CornerIllustrations.tsx
@@ -4,12 +4,16 @@ import corner2 from "../../assets/corner-2.png";
 export default function CornerIllustrations() {
     return (
         <>
-            <div className="corner-illustration left z-index-n1 fixed bottom-0 left-0 w-1/2 h-1/2 -mt-16 -ml-16 scale-50">
-                <img src={corner1} alt="Illustration Corner 1" />
-            </div>
-            <div className="corner-illustration right z-index-n1 fixed bottom-0 right-0 w-1/2 h-1/2 -mb-16 -mr-16 scale-50">
-                <img src={corner2} alt="Illustration Corner 2" />
-            </div>
+            <img
+                src={corner1}
+                alt="Illustration Corner 1"
+                className="corner-illustration left z-index-n1 fixed bottom-0 left-0"
+            />
+            <img
+                src={corner2}
+                alt="Illustration Corner 2"
+                className="corner-illustration right z-index-n1 fixed bottom-0 right-0"
+            />
         </>
     );
 }
